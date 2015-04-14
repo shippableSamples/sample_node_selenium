f5aecb273520a9ace3d91f5ad1d27a137c176760
CADBOT <charliec364@gmail.com>
2015-04-14 10:38:11 -0500

    make a change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><head></head><body>Bye World</body></html>');
+  res.end('<html><head></head><body>Hey there World</body></html>');
   // Prevents the build container from timing out by waiting for the server to close.
   process.nextTick(function () {
     console.log('Stopping server running at http://127.0.0.1:1337');
