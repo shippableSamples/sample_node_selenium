565837b5c2efbbaa4b1bb7b1d35390354fc09256
balavignesh-s <balavignesh@shippable.com>
2016-02-23 13:56:28 +0530

    client expects something and server sends something else - fixed

--- tests/sample_test.js
+++ tests/sample_test.js
@@ -1,7 +1,7 @@
 module.exports = {
   "test sample" : function (browser) {
     browser
-      .url("http://127.0.0.1:1337")
+      .url("http://127.0.0.1:5001")
       .waitForElementVisible('body', 1000)
       .assert.containsText('body', 'Hello World')
       .end();
