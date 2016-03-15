e6ad9ad4a2ef8193f1c4f0d39d163bc87c0da28e
chetantarale <chetan@shippable.com>
2016-03-15 15:21:42 +0530

    Update sample_test.js

--- tests/sample_test.js
+++ tests/sample_test.js
@@ -3,6 +3,9 @@ module.exports = {
     browser
       .url("http://127.0.0.1:5001")
       .waitForElementVisible('body', 1000)
+      .title(function(err, res) {
+        console.log('Title was: ' + navigator.userAgent); // Confirm the page title
+      })
       .assert.containsText('body', 'Hello World')
       .end();
   }
