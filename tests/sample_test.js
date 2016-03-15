ee015418048fae2d64232cf58ae61a72ec4cf715
chetantarale <chetan@shippable.com>
2016-03-15 15:29:16 +0530

    Update sample_test.js

--- tests/sample_test.js
+++ tests/sample_test.js
@@ -3,7 +3,7 @@ module.exports = {
     browser
       .url("http://127.0.0.1:5001")
       .waitForElementVisible('body', 1000)
-      .assert.containsText('body', 'Linux x86_64')
+      .assert.containsText('p', 'Linux x86_64')
       .end();
   }
 };
