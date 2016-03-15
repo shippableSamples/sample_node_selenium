b0d0471777733d4eb94a69cc465c1477821de2b8
chetantarale <chetan@shippable.com>
2016-03-15 16:05:51 +0530

    Update sample_test.js

--- tests/sample_test.js
+++ tests/sample_test.js
@@ -3,7 +3,7 @@ module.exports = {
     browser
       .url("http://127.0.0.1:5001")
       .waitForElementVisible('body', 1000)
-      .assert.containsText('body', "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36")
+      .assert.containsText('body', "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36")
       .end();
   }
 };
